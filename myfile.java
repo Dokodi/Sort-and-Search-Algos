0c0a8eaccf7fdd4cc30ea5f52d0c2f6454517f07
Dokodi Kelvin <85605125+Dokodi@users.noreply.github.com>
2021-10-07 16:08:35 +0100

    Create myfile.java

---
+++ myfile.java
@@ -0,0 +1,23 @@
+// Shell sort in Java programming
+
+import java.util.Arrays;
+
+
+class ShellSort {
+
+  void shellSort(int array[], int n) {
+  for (int interval = n / 2; interval > 0; interval /= 2) {
+    for (int i = interval; i < n; i += 1) {
+    int temp = array[i];
+    int j;
+    for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+      array[j] = array[j - interval];
+    }
+    array[j] = temp;
+    }
+   
+  System.out.println(array[])
+  }
+  }
+
+  
